6d6c85c4a1d36e1ea8136374f6af53f05e7a113c
marcelinho21junior <marcelo.belitardo@fatec.sp.gov.br>
2022-05-16 17:12:10 -0300

    commit first

---
+++ funcoes.mjs
@@ -0,0 +1,13 @@
+function mostrarIdade(nome, idade){
+    return `A idade de ${nome} é ${idade}`
+}
+
+function mostrarCidade(nome, idade){
+    return `A Cidade de ${nome} é ${idade}`
+}
+
+function mostrarHoby(nome, idade){
+    return `O hobby de ${nome} é ${idade}`
+}
+
+export {mostrarIdade,mostrarCidade,mostrarHoby}
---
+++ main.mjs
@@ -0,0 +1,5 @@
+import { mostrarIdade,mostrarCidade,mostrarHoby } from "./funcoes.mjs";
+
+console.log(mostrarIdade("Marcelo",27))
+console.log(mostrarCidade("Marcelo","Lorena"))
+console.log(mostrarHoby("Marcelo","Ler livros"))
